18c045f8b1abeb914ff4eb0a65336e762dc76519
KevinTender <tenderkevin@gmail.com>
2020-12-06 23:43:22 +0300

    сделан прототип мехники игры. управление персонажем

---
+++ Assets/MoveControler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MoveControler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public GameObject pointPrefab;
+    public GameObject playerObj;
+    Vector3 startMoveV;
+    Vector3 endMoveV;
+    public void OnDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startMoveV = eventData.pointerCurrentRaycast.worldPosition;
+        Debug.Log("START: " + startMoveV);
+        //Instantiate(pointPrefab, startMoveV, Quaternion.identity);
+
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        endMoveV = eventData.pointerCurrentRaycast.worldPosition;
+        Debug.Log("END: " + endMoveV);
+        //Instantiate(pointPrefab, endMoveV, Quaternion.identity);
+        Vector3 moveV = endMoveV - startMoveV;
+        RaycastHit2D hit = Physics2D.Raycast(playerObj.transform.position,  moveV);
+        if (hit.collider != null)
+        {
+            Instantiate(pointPrefab, hit.point, Quaternion.identity);
+            playerObj.GetComponent<PlayerControler>().SetGoal(hit.point);
+        }
+    }
+}
---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public Vector3 goal;
+    public float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position,goal) > 1)
+        {
+            speed = 15;
+        }
+        else
+        {
+            speed = 1;
+        }
+        transform.position = Vector3.Lerp(transform.position,goal,Time.deltaTime * speed);
+    }
+    public void SetGoal(Vector3 newGoal)
+    {
+        goal = newGoal;
+    }
+}
