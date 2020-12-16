1762007f80e3bcaebb3f7dc87121b778cd422a6f
KevinTender <tenderkevin@gmail.com>
2020-12-16 12:08:35 +0300

    Проверка работы Rider

--- Assets/Scripts/Levels/LevelControler.cs
+++ Assets/Scripts/Levels/LevelControler.cs
@@ -7,7 +7,7 @@ public class LevelControler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3();
     }
 
     // Update is called once per frame
